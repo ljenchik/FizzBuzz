10b80b2e5cb034bf9bf29387c3c4a2b3f3a9d891
ljenchik <pelagenkolena@gmail.com>
2022-04-26 13:27:54 +0100

    Added Bong

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,10 +1,14 @@
 function FizzBuzz() {
     let divisors = [3, 5, 7]
     let words = ["Fizz", "Buzz", "Bang"]
-    
     for (let i = 1; i <= 100;  i++)
     {
         let toPrint = ""
+        if (i % 11 === 0) 
+        {
+            toPrint = 'Bong'
+            continue;
+        }
         for (let j = 0; j < divisors.length; j++)
         {
             if (i % divisors[j] === 0) 
