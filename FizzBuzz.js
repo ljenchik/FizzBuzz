cdb6d4012a27fd2746f6555a563fda51d9219ca0
ljenchik <pelagenkolena@gmail.com>
2022-04-26 15:41:05 +0100

    Added multiples of 11, 13, 17

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,30 +1,47 @@
-function FizzBuzz() {
-    let divisors = [3, 5, 7]
-    let words = ["Fizz", "Buzz", "Bang"]
-    for (let i = 1; i <= 100;  i++)
-    {
-        let toPrint = ""
-        if (i % 11 === 0) 
-        {
-            toPrint = 'Bong'
-            continue;
-        }
-        for (let j = 0; j < divisors.length; j++)
-        {
-            if (i % divisors[j] === 0) 
-            {
-                toPrint += words[j] 
-            }
-        }
-        if (toPrint === "") 
-        {
-            console.log(i)
-        }
-        else {
-            console.log(toPrint)
+function indexOfB(arr)
+{
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].startsWith("B")) {
+            return i
         }
     }
+    return -1
 }
-
+    
+function FizzBuzz() {
+    for (let i = 1; i <= 255; i++) {
+        let result = []
+        if (i % 3 === 0) {
+            result.push("Fizz")
+        }
+        if (i % 5 === 0) {
+            result.push("Buzz")
+        }
+        if (i % 7 === 0) {
+            result.push("Bang")  
+        }
+        if (i % 11 === 0) {
+            result = ["Bong"]
+        }
+        if (i % 13 === 0) {
+            if (indexOfB(result) === -1) 
+            {
+                result.push("Fezz")
+            }
+            else 
+            {
+                result.splice(indexOfB(result), 0, "Fezz")
+            }
+        }
+        if (i % 17 === 0) {
+            result.reverse();
+        }
+        if (result.length === 0) 
+        {
+        result.push(i.toString());
+        }
+    console.log(result.join(""))
+    }
+}
 
 FizzBuzz();
