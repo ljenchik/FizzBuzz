64a2191ceec9ac94a680d733af5fe6b95a684816
ljenchik <pelagenkolena@gmail.com>
2022-04-26 11:15:55 +0100

    Minor error

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,4 +1,4 @@
-function FizzBUzz() {
+function FizzBuzz() {
     for (let i = 1; i <= 100; i ++){
         if (i % 3 === 0 && i % 5 === 0) {
             console.log("FizzBuzz")
@@ -15,4 +15,4 @@ function FizzBUzz() {
     }
 }
 
-FizzBUzz();
+FizzBuzz();
